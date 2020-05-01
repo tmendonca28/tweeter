ead4f3b69ddea6f1dd26cf859b87875d3e5938fd
tmendonca28 <tmendonca28@gmail.com>
2020-05-01 15:22:06 +0100

    Adding base html to templates and block content, dynamic js on home.html
    Signed-off-by: tmendonca28 <tmendonca28@gmail.com>

--- tweets/views.py
+++ tweets/views.py
@@ -8,6 +8,21 @@ def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
 
 
+def tweet_list_view(request, *args, **kwargs):
+    """
+    REST API VIEW
+    Consume by JS
+    return json data
+    """
+    qs = Tweet.objects.all()
+    tweets_list = [{"id": x.id, "content": x.content} for x in qs]
+    data = {
+        "isUser": False,
+        "response": tweets_list
+    }
+    return JsonResponse(data)
+
+
 def tweet_details_view(request, tweet_id, *args, **kwargs):
     """
     REST API VIEW
