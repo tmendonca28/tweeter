b001f184084e72d8903f054f04b545d3826c7a9c
tmendonca28 <tmendonca28@gmail.com>
2020-05-21 14:20:16 +0100

    Adding permissions and authentication decorators for django rest framework
    Signed-off-by: tmendonca28 <tmendonca28@gmail.com>

--- tweets/views.py
+++ tweets/views.py
@@ -4,7 +4,9 @@ from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render, redirect
 from django.utils.http import is_safe_url
 
-from rest_framework.decorators import api_view
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .forms import TweetForm
 from .models import Tweet
@@ -18,6 +20,8 @@ def home_view(request, *args, **kwargs):
 
 
 @api_view(['POST']) # http method from client == POST
+# @authentication_classes([SessionAuthentication])
+@permission_classes([IsAuthenticated])
 def tweet_create_view(request, *args, **kwargs):
     serializer = TweetSerializer(data=request.POST)
     if serializer.is_valid(raise_exception=True):
